0c92d573b9d909e2d88c5eedee3c926096551bd0
vicsanse <vicsanse@alumni.uv.es>
2016-07-13 16:11:37 +0200

    Terminada la funcion que llama a get_responses

--- sipm_responses.C
+++ sipm_responses.C
@@ -20,46 +20,43 @@ void run() {
   // Generate SiPM responses for each grid point and save
   // the response array to a TTree.
   
-
-  
-  int N = 1; // photons
+  int N = 1; // Number of photons
   
   double max_xy = 7*sipm_pitch + 2*sipm_edge_width; // maximum x and y value (80 mm)
   
   double max_p = max_xy /grid_space; // number of points per line (40)
   
-  for(double x=0; x<(max_p);x++){
+  TFile f("simulation_N_1.root"); // TFile in which to store the tree.
+  
+  TTree * tr = new TTree("sim_responses","Locations  of  the  grid  points  and  the responses of the SiPMs for each"); 
+  
+  //Variables assigned to branches: location(x,y), array of responses
+  
+  double x, y;
+  double * sipm_prob = new double[NSIPM*NSIPM];
+  
+  //Branches:
+  
+  tr->Branch("x", &x, "x/I"); // /I??
+  tr->Branch("y", &y, "y/I");
+  tr->Branch("sipm_prob", sipm_prob, "sipm_prob[NSIPM*NSIPM]/D");
+  
+  for(x=0; x<(max_p);x++){
 	  
-	  for(double y=0; y<(max_p);y++){
-		  x = x*grid_space + grid_space/2;
-		  y = y*grid_space + grid_space/2;
-		  double * sipm_prob = new double[NSIPM*NSIPM];
+	  for(y=0; y<(max_p);y++){
+		  x = x*grid_space + grid_space/2; // half point of the grid (1,3,5..79)
+		  y = y*grid_space + grid_space/2; // half point of the grid (1,3,5..79)
+
+		  get_responses(N,x,y,sipm_prob);
 		  
-		  cout << "(" << x << ", " << y << ") " << endl; // llamar a la función get_responses(N,x,y,sipm_prob)
-		  //get_responses(N,x,y,sipm_prob);
+		  tr->Fill(); //Saves data on the tree
 		  
+		  // So that the loop keeps working with x, y: 0,1,2..39 we undo the changes of these variables:
 		  x = (x - grid_space/2)/grid_space;
 		  y = (y - grid_space/2)/grid_space;
 		   
 		  }
   }
-  
- /* for(double i=0; i<pow(max_p,2); i++){
-	  
-	  int p = i % max_p;
-	  
-	  double y = p*grid_space+1; 
-	  double x = floor(i/max_p)*max_p*grid_space-1;
-	  
-	  double * sipm_prob = new double[NSIPM*NSIPM];
-	  
-
-	  
-	  cout << "(" << x << ", " << y << ") " << endl;
-	 
-	  
-  }*/
-  
-  
+  f.Close() // Close the file
 }
 	
